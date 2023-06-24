34b494cd88171c6278d0c715ef28e6a9e840ed57
jacob-finley <jacobfinley@unomaha.edu>
2023-06-23 19:46:02 -0500

    Updated repo to version 1.1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import questions from './questions';
+import Quiz from './quiz';
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <h1>College Degree Helper</h1>
+            <Quiz />
+        </div>
+    );
+    
+}
+
+export default App;
